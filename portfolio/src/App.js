ea7c0512e883b7e73d8aaa58a04e64f5686d7361
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2019-08-13 00:23:02 -0400

    bkg gradient animated plus logos

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { BrowserRouter} from 'react-router-dom';
 import Navbar from "./components/layout/Navbar";
+import Background from "./components/layout/Background";
 
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar></Navbar>
+          <Background />
         </div>
       </BrowserRouter>
     );
---
+++ portfolio/src/components/layout/Background.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Background.css';
+
+const Background = () => {
+    return (
+        <div className="bckg">
+            <section>
+               
+            </section>
+            <div className="logos">
+                <div>
+                    <img src="/images/html.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/css3.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/javascript.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/jquery.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/bootstrap.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/react.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/mongodb.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/nodejs.png" className="bkg-img"></img>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Background;
