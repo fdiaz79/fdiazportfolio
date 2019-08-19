18fdac5141a4c32f5e62175fc803c4e6eedfc39f
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2019-08-19 13:48:51 -0400

    Animated background

--- portfolio/src/components/layout/Background.js
+++ portfolio/src/components/layout/Background.js
@@ -32,6 +32,76 @@ const Background = () => {
                 <div>
                     <img src="/images/nodejs.png" className="bkg-img"></img>
                 </div>
+                <div>
+                    <img src="/images/expressjs.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/firebase.png" className="bkg-img"></img>
+                </div>
+            </div>
+            <div className="logos logos2">
+                <div>
+                    <img src="/images/react.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/css3.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/bootstrap.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/javascript.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/html.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/mongodb.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/nodejs.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/expressjs.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/firebase.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/jquery.png" className="bkg-img"></img>
+                </div>
+            </div>
+            <div className="logos logos3">
+                <div>
+                    <img src="/images/firebase.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/html.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/css3.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/javascript.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/jquery.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/bootstrap.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/react.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/mongodb.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/nodejs.png" className="bkg-img"></img>
+                </div>
+                <div>
+                    <img src="/images/expressjs.png" className="bkg-img"></img>
+                </div>
             </div>
         </div>
     )
