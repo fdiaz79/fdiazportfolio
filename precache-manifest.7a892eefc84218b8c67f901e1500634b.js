ee46d6d29e02e53416bacebe98d86e4a57a640df
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2019-10-16 14:02:05 -0400

    Updates

--- precache-manifest.7a892eefc84218b8c67f901e1500634b.js
+++ precache-manifest.7a892eefc84218b8c67f901e1500634b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4270c30c8bfa62e9acff45f33b31c8a7",
+    "revision": "27165d680bfaa76aaa456ab7f91029cb",
     "url": "/portfolioReact/index.html"
   },
   {
-    "revision": "803e183b53e991b2c979",
+    "revision": "44df53ebcc579c90991b",
     "url": "/portfolioReact/static/css/main.e1c28549.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolioReact/static/js/2.54b9a1b1.chunk.js"
   },
   {
-    "revision": "803e183b53e991b2c979",
-    "url": "/portfolioReact/static/js/main.880a42d8.chunk.js"
+    "revision": "44df53ebcc579c90991b",
+    "url": "/portfolioReact/static/js/main.f04d6bb5.chunk.js"
   },
   {
     "revision": "9028f188735ef4f1d2ea",
