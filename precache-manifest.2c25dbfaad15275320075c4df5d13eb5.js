9bee695304e4ecd3138a9a38710412f14120971f
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2019-10-16 13:18:51 -0400

    Updates

---
+++ precache-manifest.2c25dbfaad15275320075c4df5d13eb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c4c478a7d9de8f63b6aa45242df35b",
+    "url": "/fdiazportfolio/index.html"
+  },
+  {
+    "revision": "80dfdce79219528b604c",
+    "url": "/fdiazportfolio/static/css/main.e1c28549.chunk.css"
+  },
+  {
+    "revision": "5e656d3b343bf263b467",
+    "url": "/fdiazportfolio/static/js/2.982bb0bf.chunk.js"
+  },
+  {
+    "revision": "80dfdce79219528b604c",
+    "url": "/fdiazportfolio/static/js/main.40692dc6.chunk.js"
+  },
+  {
+    "revision": "127d43a730671a0d10a1",
+    "url": "/fdiazportfolio/static/js/runtime~main.ab1857d8.js"
+  }
+]);
