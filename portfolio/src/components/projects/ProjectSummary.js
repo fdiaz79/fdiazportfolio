3a817f275f717002dacf19458c87bc44fa204a98
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2019-10-23 21:08:50 -0400

    footer hidden to the right in small screens. proj description  overflow scroll

--- portfolio/src/components/projects/ProjectSummary.js
+++ portfolio/src/components/projects/ProjectSummary.js
@@ -22,29 +22,6 @@ const ProjectSummary = ({project}) => {
                 </div>    
             </div>
         </div>
-
-        // <div className="col-12 col-md-3 card-cont">
-        //     <div className="proj-card middle">
-        //         <div className="front">
-        //             <img className="proj-img img-fluid" src={project.image} alt={project.name}></img>
-        //         </div>
-        //         <div className="back">
-        //             <div className="proj-desc middle">
-        //                 <h5 className="title">{project.name}</h5>
-        //                 <p className="descript">{project.description} </p>
-        //                 <p className="tech">TECHNOLOGIES: <br /> {project.tech} </p>
-        //                 <div className="sm">                          
-        //                     <p>Added on: {moment(project.createdAt.toDate()).calendar()} </p>
-        //                     <br></br>
-        //                     <p className="logoLink">
-        //                         <a href={project.link} target="_blank" rel="noopener noreferrer"> <img width="30px" src="/images/navigate.jpg" alt="navigate" /> </a>
-        //                         <a href={project.hub} target="_blank" rel="noopener noreferrer"> <img width="30px" src="/images/github.png" alt="github" /> </a>
-        //                     </p>                                                      
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
     )
 }
 
