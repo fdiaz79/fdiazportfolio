db27feda43ccb3b0d2e8309ae16fcf5fcb051ee6
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2019-10-24 21:03:18 -0400

    authorization to see message list

--- portfolio/src/components/contact/MessageList.js
+++ portfolio/src/components/contact/MessageList.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import MessageSummary from './MessageSummary';
+import { connect } from 'react-redux';
 
 
-const MessageList = ({messages}) => {
+const MessageList = ({ messages, auth }) => {
+    // console.log(auth);
+    // const { messages } = props;
+    const list = auth.uid ?  messages && messages.map(message => {
+        return(
+            <MessageSummary message={message} key={message.id} />
+        )
+    }) :  <p>Please Sign in to be able to see the messages</p>
     return(
         <div id="message-list">
             <h5>MESSAGE LIST</h5>
-            { messages && messages.map(message => {
+            {/* { messages && messages.map(message => {
                 return(
                     <MessageSummary message={message} key={message.id} />
                 )
-            })}
+            })} */}
+            { list }
         </div>
             
         
     )
 }
-export default MessageList;
+
+const mapStateToProps=(state) => {
+    // console.log(state);
+    return{
+        auth: state.firebase.auth
+    }
+}
+export default connect(mapStateToProps)(MessageList);
